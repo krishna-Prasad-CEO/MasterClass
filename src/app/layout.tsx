c1f9c643ce6aa204e8a3804d11ac54c98f4b7902
krishna-Prasad-CEO <krishnaprasadandco12@gmail.com>
2025-07-09 21:21:59 +0530

    slkfdjsdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
